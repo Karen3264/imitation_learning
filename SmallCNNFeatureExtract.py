40aa5b74ae5f82482c9f869ad8207f5f2ebc375a
Karen3264 <125595579+Karen3264@users.noreply.github.com>
2025-09-22 16:19:50 +0200

    Create SmallCNNFeatureExtract.py

---
+++ SmallCNNFeatureExtract.py
@@ -0,0 +1,25 @@
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+import torch
+from torch import nn
+
+#smaller CNN from https://minigrid.farama.org/content/training/
+class SmallCNN(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.Space, features_dim: int = 512, normalized_image: bool = False) -> None:
+        super().__init__(observation_space, features_dim)
+        n_input_channels = observation_space.shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 16, (1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, (1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, (1, 1)),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+        # Compute shape by doing one forward pass
+        with torch.no_grad():
+            n_flatten = self.cnn(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        return self.linear(self.cnn(observations))
